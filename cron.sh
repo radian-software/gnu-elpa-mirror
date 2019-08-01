7a842d70526e76246c07aa959b7197ed9b45cada
Radon Rosborough <radon.neon@gmail.com>
2019-07-31 18:24:02 -0700

    Run more frequently

--- cron.sh
+++ cron.sh
@@ -5,8 +5,11 @@ set -o pipefail
 
 time="$(date +%I:%M)"
 
-if [[ "$1" != "-f" ]] && ! [[ "$time" > "05:30" && "$time" < "06:30" ]]; then
-    echo "exiting as it is not 6am or 6pm; pass -f to override" 1>&2
+if [[ "$1" != "-f" ]] && ! (
+           [[ "$time" > "02:30" && "$time" < "03:30" ]] ||
+               [[ "$time" > "05:30" && "$time" < "06:30" ]] ||
+               [[ "$time" > "08:30" && "$time" < "09:30" ]]); then
+    echo "exiting as it is not a scheduled time; pass -f to override" 1>&2
     exit 1
 fi
 
