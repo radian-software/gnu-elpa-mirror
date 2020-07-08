23104e0d638321dc53bedb46cb688bffe7bc8687
Radon Rosborough <radon.neon@gmail.com>
2020-07-08 12:02:32 -0600

    Fourth try

--- gnu_elpa_mirror.py
+++ gnu_elpa_mirror.py
@@ -333,7 +333,9 @@ def mirror_emacsmirror(args, api, existing_repos):
                     r'\[submodule "[^"]+"\]\n|'
                     r"\tpath = .+\n|"
                     r"\turl = git@github.com:([^/]+)/(.+)\.git\n|"
+                    r"\turl = git@github.com:emacsmirror/emacswiki.org.git\n|"
                     r"\turl = https://git.savannah.gnu.org/git/emacs/elpa.git\n|"
+                    r"\turl = git@github.com:melpa/melpa.git\n|"
                     r"\tbranch = .+\n",
                     line,
                 )
