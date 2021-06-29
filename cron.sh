6183da18dcece9021ce79e8226a453e868a2abbd
Radon Rosborough <radon.neon@gmail.com>
2021-06-28 22:33:27 -0700

    Decrease scrape frequency
    Should help with my Heroku bill

--- cron.sh
+++ cron.sh
@@ -3,14 +3,4 @@
 set -e
 set -o pipefail
 
-time="$(date +%I:%M)"
-
-if [[ "$1" != "-f" ]] && ! (
-           [[ "$time" > "02:30" && "$time" < "03:30" ]] ||
-               [[ "$time" > "05:30" && "$time" < "06:30" ]] ||
-               [[ "$time" > "08:30" && "$time" < "09:30" ]]); then
-    echo "exiting as it is not a scheduled time; pass -f to override" 1>&2
-    exit 1
-fi
-
 python3 -u -m gnu_elpa_mirror
