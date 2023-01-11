9d7248e10d05fd4c9cc21592f047dcd6fb89cee4
Radon Rosborough <radon@intuitiveexplanations.com>
2023-01-11 16:47:54 -0700

    Reduce billing

--- cron.py
+++ cron.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 
 import datetime
+import os
 import subprocess
+import sys
 import time
 
 import croniter
 
 # Every day at 12am UTC (or system timezone)
 for next_time in croniter.croniter(
-    "0 0 * * *", datetime.datetime.now(), ret_type=datetime.datetime
+    "0 0 */3 * *", datetime.datetime.now(), ret_type=datetime.datetime
 ):
     if datetime.datetime.now() > next_time:
         # Last invocation took so long we passed the next time to
@@ -20,3 +22,8 @@ for next_time in croniter.croniter(
         time.sleep(60)
     # Ignore errors.
     subprocess.run(["./gnu_elpa_mirror.py"])
+    # Work around an issue that I'm seeing on Railway where memory
+    # utilization doesn't go away even after finishing the run.
+    if os.environ.get("CRON_FORCE_CONTAINER_RESTART") == "1":
+        print("cron.py: Forcing container restart to workaround memory leak")
+        sys.exit(1)
