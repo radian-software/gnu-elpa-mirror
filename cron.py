857b312ddef1433ecfc33c70324b6e1fd314e816
Radon Rosborough <radon@intuitiveexplanations.com>
2023-02-15 18:42:13 -0800

    [#14] Run every day again

--- cron.py
+++ cron.py
@@ -10,7 +10,7 @@ import croniter
 
 # Every day at 12am UTC (or system timezone)
 for next_time in croniter.croniter(
-    "0 0 */3 * *", datetime.datetime.now(), ret_type=datetime.datetime
+    "0 0 * * *", datetime.datetime.now(), ret_type=datetime.datetime
 ):
     if datetime.datetime.now() > next_time:
         # Last invocation took so long we passed the next time to
