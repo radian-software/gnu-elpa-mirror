accb598ef5dcc593412706add48749cfe3f7941a
Radon Rosborough <radon@intuitiveexplanations.com>
2024-05-05 09:40:11 -0700

    Rewrite to pull from published tarballs instead

--- gnu_elpa_mirror.py
+++ gnu_elpa_mirror.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 
 import argparse
-import datetime
+from dataclasses import dataclass
+from datetime import datetime
 import json
 import os
-import pathlib
+from pathlib import Path
 import re
 import shutil
 import subprocess
 import sys
+import tempfile
+from typing import Any
 
 import dotenv
 import github
@@ -207,7 +210,7 @@ def stage_and_commit(repo_dir, message):
                 "-c",
                 "user.name=GNU ELPA Mirror Bot",
                 "-c",
-                "user.email=emacs-devel@gnu.org",
+                "user.email=contact+gnu-elpa-mirror@radian.codes",
                 "commit",
                 "-m",
                 message,
@@ -219,141 +222,130 @@ def stage_and_commit(repo_dir, message):
         log("(no changes)")
 
 
-# https://savannah.gnu.org/git/?group=emacs
-GNU_ELPA_GIT_URL = "https://git.savannah.gnu.org/git/emacs/elpa.git"
-EMACS_GIT_URL = "https://git.savannah.gnu.org/git/emacs.git"
-
-REPOS_SUBDIR = pathlib.Path("repos")
-GNU_ELPA_SUBDIR = REPOS_SUBDIR / "elpa"
-GNU_ELPA_PACKAGES_SUBDIR = GNU_ELPA_SUBDIR / "packages"
-EMACS_SUBDIR = GNU_ELPA_SUBDIR / "emacs"
+THIS_DIR = Path(".").resolve()
+REPOS_SUBDIR = THIS_DIR / "repos"
+GNU_ELPA_SUBDIR = THIS_DIR / "gnu-elpa"
 
 
-def make_commit_message(message, data):
-    return (
-        "{}\n\n"
-        "Timestamp: {}\n"
-        "GNU ELPA commit: {}\n"
-        "Emacs commit: {}".format(
-            message, data["timestamp"], data["gnu_elpa_commit"], data["emacs_commit"]
-        )
-    )
+@dataclass
+class ELPAPackage:
+    name: str
+    version: str
+
+    @property
+    def tarball_name(self) -> str:
+        return f"{self.name}-{self.version}.tar"
+
+    @property
+    def tarball_url(self) -> str:
+        return f"https://elpa.gnu.org/devel/{self.tarball_name}"
 
 
-def mirror_gnu_elpa(args, api, existing_repos):
-    log("--> clone/update GNU ELPA")
-    clone_git_repo(
-        GNU_ELPA_GIT_URL,
-        GNU_ELPA_SUBDIR,
-        private_url=False,
-        exclude_patterns=["/emacs"],
-    )
-    subprocess.run(
-        ["git", "remote", "remove", "origin"],
-        cwd=GNU_ELPA_SUBDIR,
-        check=False,
-    )
-    try:
-        shutil.rmtree(GNU_ELPA_SUBDIR / ".git" / "worktrees")
-    except FileNotFoundError:
-        pass
-    subprocess.run(
-        ["git", "remote", "add", "origin", GNU_ELPA_GIT_URL],
-        cwd=GNU_ELPA_SUBDIR,
-        check=True,
-    )
-    with open(GNU_ELPA_SUBDIR / "Makefile", "r+") as f:
-        text = f.read().replace("git worktree add -b", "git worktree add -B")
-        f.seek(0)
-        f.write(text)
-        f.truncate()
-    log("--> clone/update Emacs")
-    clone_git_repo(
-        EMACS_GIT_URL,
-        EMACS_SUBDIR,
-        private_url=False,
-    )
-    log("--> check timestamp and commit hashes")
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    brief_timestamp = datetime.datetime.now().strftime("%Y-%m-%d")
-    gnu_elpa_commit = (
-        subprocess.run(
-            ["git", "rev-parse", "HEAD"],
-            cwd=GNU_ELPA_SUBDIR,
-            stdout=subprocess.PIPE,
-            check=True,
-        )
-        .stdout.decode()
-        .strip()
-    )
-    emacs_commit = (
-        subprocess.run(
-            ["git", "rev-parse", "HEAD"],
-            cwd=EMACS_SUBDIR,
-            stdout=subprocess.PIPE,
-            check=True,
-        )
-        .stdout.decode()
-        .strip()
-    )
-    commit_data = {
-        "timestamp": timestamp,
-        "gnu_elpa_commit": gnu_elpa_commit,
-        "emacs_commit": emacs_commit,
-    }
-    elpa_config = json.loads(
+def make_commit_message(
+    message: str, timestamp: datetime, pkg: ELPAPackage | None = None
+):
+    message += f"\n\nTimestamp: {timestamp.strftime('%Y-%m-%d %H:%M:%S')}"
+    if pkg:
+        message += f"\nSourced from {pkg.name} version {pkg.version} on GNU ELPA Devel"
+        message += f"\n(see https://elpa.gnu.org/devel/{pkg.name}.html)"
+    return message
+
+
+def read_elpa_index(path: Path) -> Any:
+    # WARNING: path basename can't have special chars
+    path = path.resolve()
+    return json.loads(
         subprocess.run(
             [
                 "emacs",
                 "-Q",
                 "--batch",
                 "-l",
+                "package",
+                "-l",
                 "json",
                 "--eval",
-                """\
+                f"""\
 (with-temp-buffer
-  (insert-file-contents "elpa-packages")
-  (princ (json-encode (read (current-buffer)))))
+  (insert-file-contents "{path.name}")
+  (princ
+   (json-encode
+    (mapcar
+     (lambda (spec)
+       (cons
+        (car spec)
+        (package-version-join (aref (cdr spec) 0))))
+    (seq-filter #'listp (read (current-buffer)))))))
 """,
             ],
             stdout=subprocess.PIPE,
-            cwd=GNU_ELPA_SUBDIR,
+            cwd=path.parent,
         ).stdout.decode()
     )
-    log("--> retrieve/update GNU ELPA external packages")
-    subprocess.run(["make", "setup", "-f", "Makefile"], cwd=GNU_ELPA_SUBDIR, check=True)
-    subprocess.run(["make", "worktrees"], cwd=GNU_ELPA_SUBDIR, check=True)
-    packages = []
-    for subdir in sorted(GNU_ELPA_PACKAGES_SUBDIR.iterdir()):
-        if not subdir.is_dir():
+
+
+def get_elpa_contents(archive_url: str) -> list[ELPAPackage]:
+    if not archive_url.endswith("/"):
+        archive_url += "/"
+    resp = requests.get(archive_url + "archive-contents")
+    resp.raise_for_status()
+    with tempfile.NamedTemporaryFile("w") as f:
+        f.write(resp.text)
+        f.flush()
+        data = read_elpa_index(Path(f.name))
+    return [
+        ELPAPackage(name, version)
+        for name, version in data.items()
+        # Denylist some special names to make sure there is nothing
+        # unrelated that gets accidentally overwritten by somebody
+        # publishing a naughty package on GNU ELPA.
+        if name
+        not in {"gnu-elpa-mirror", "epkgs", "emacsmirror-mirror", "org-mode", "elpa"}
+    ]
+
+
+def mirror_gnu_elpa(args, api, existing_repos):
+    package_filter = (
+        lambda name: name == args.mirror_only_one or not args.mirror_only_one
+    )
+    log("--> check timestamp and commit hashes")
+    timestamp = datetime.now()
+    commit_data = {
+        "timestamp": timestamp,
+    }
+    log("--> check GNU ELPA archive index")
+    elpa_packages = get_elpa_contents("https://elpa.gnu.org/devel/")
+    log("--> download GNU ELPA tarballs")
+    GNU_ELPA_SUBDIR.mkdir(exist_ok=True)
+    existing_tarballs = set(os.listdir(GNU_ELPA_SUBDIR))
+    for pkg in elpa_packages:
+        if not package_filter(pkg.name):
             continue
-        # Prevent monkey business.
-        if subdir.name in (
-            "gnu-elpa-mirror",
-            "epkgs",
-            "emacsmirror-mirror",
-            "org-mode",
-            "elpa",
-        ):
+        if pkg.tarball_name in existing_tarballs:
             continue
-        packages.append(subdir.name)
+        log(f"----> download {pkg.tarball_url}")
+        resp = requests.get(pkg.tarball_url, stream=True)
+        resp.raise_for_status()
+        with open(GNU_ELPA_SUBDIR / pkg.tarball_name, "wb") as f:
+            for chunk in resp.iter_content(10 * 1024):
+                f.write(chunk)
     log("--> clone/update mirror repositories")
     org = api.get_organization("emacs-straight")
     REPOS_SUBDIR.mkdir(exist_ok=True)
-    for package in packages:
-        if args.mirror_only_one and package != args.mirror_only_one:
+    for pkg in elpa_packages:
+        if not package_filter(pkg.name):
             continue
-        github_package = package.replace("+", "-plus")
+        github_package = pkg.name.replace("+", "-plus")
         git_url = "https://raxod502:{}@github.com/emacs-straight/{}.git".format(
             ACCESS_TOKEN, github_package
         )
-        repo_dir = REPOS_SUBDIR / package
+        repo_dir = REPOS_SUBDIR / pkg.name
         if github_package not in existing_repos:
-            log("----> create mirror repository {}".format(package))
+            log("----> create mirror repository {}".format(pkg.name))
             org.create_repo(
                 github_package,
-                description=("Mirror of the {} package from GNU ELPA".format(package)),
-                homepage=("https://elpa.gnu.org/packages/{}.html".format(package)),
+                description=("Mirror of the {} package from GNU ELPA".format(pkg.name)),
+                homepage=("https://elpa.gnu.org/packages/{}.html".format(pkg.name)),
                 has_issues=False,
                 has_wiki=False,
                 has_projects=False,
@@ -361,27 +353,26 @@ def mirror_gnu_elpa(args, api, existing_repos):
             )
         if args.skip_mirror_pulls and repo_dir.is_dir():
             continue
-        log("----> clone/update mirror repository {}".format(package))
+        log("----> clone/update mirror repository {}".format(pkg.name))
         clone_git_repo(git_url, repo_dir, private_url=True)
     log("--> update mirrored packages")
-    for package in packages:
-        if args.mirror_only_one and package != args.mirror_only_one:
+    for pkg in elpa_packages:
+        if not package_filter(pkg.name):
             continue
-        log("----> update package {}".format(package))
-        package_dir = GNU_ELPA_PACKAGES_SUBDIR / package
-        repo_dir = REPOS_SUBDIR / package
+        log("----> update package {}".format(pkg.name))
+        repo_dir = REPOS_SUBDIR / pkg.name
         delete_contents(repo_dir)
-        for source in sorted(package_dir.iterdir()):
-            if source.name == ".git":
-                continue
-            target = repo_dir / source.name
-            if source.is_dir() and not source.is_symlink():
-                shutil.copytree(source, target)
-            else:
-                is_relative_symlink = source.is_symlink() and str(
-                    source.resolve()
-                ).startswith(str(package_dir.resolve()))
-                shutil.copyfile(source, target, follow_symlinks=not is_relative_symlink)
+        subprocess.run(
+            [
+                "tar",
+                "-C",
+                str(repo_dir),
+                "-xf",
+                str(GNU_ELPA_SUBDIR / pkg.tarball_name),
+                "--strip-components=1",
+            ],
+            check=True,
+        )
         # Remove files that may make GitHub interpret this repo
         # specially, as it should just be a static fork with the
         # packaging files.
@@ -396,82 +387,58 @@ def mirror_gnu_elpa(args, api, existing_repos):
             f.write(
                 ":warning: This repo is a read-only mirror. Please submit changes upstream instead :warning:\n"
             )
-        # Check for custom lisp-dir and copy files to top level if needed
-        # https://github.com/radian-software/gnu-elpa-mirror/issues/7
-        #
-        # Note the check for a list is because some of the entries in
-        # the elpa-packages datastructure are malformed, because of
-        # course they are, and json-serialize into unexpected types.
-        if isinstance(elpa_config.get(package), dict) and (
-            lisp_dir_name := elpa_config[package].get("lisp-dir")
-        ):
-            lisp_dir = repo_dir / lisp_dir_name
-            for source in sorted(lisp_dir.iterdir()):
-                target = repo_dir / source.name
-                if target.name == lisp_dir.name:
-                    continue
-                if source.is_dir() and not source.is_symlink():
-                    shutil.copytree(source, target)
-                else:
-                    is_relative_symlink = source.is_symlink() and str(
-                        source.resolve()
-                    ).startswith(str(repo_dir.resolve()))
-                    shutil.copyfile(
-                        source, target, follow_symlinks=not is_relative_symlink
-                    )
-            shutil.rmtree(lisp_dir)
         stage_and_commit(
-            repo_dir, make_commit_message("Update " + package, commit_data)
+            repo_dir, make_commit_message("Update " + pkg.name, timestamp, pkg)
         )
     if not args.skip_mirror_pushes:
         log("--> push changes to mirrored packages")
-        for package in packages:
-            if args.mirror_only_one and package != args.mirror_only_one:
+        for pkg in elpa_packages:
+            if not package_filter(pkg.name):
                 continue
-            log("----> push changes to package {}".format(package))
-            repo_dir = REPOS_SUBDIR / package
-            github_package = package.replace("+", "-plus")
+            log("----> push changes to package {}".format(pkg.name))
+            repo_dir = REPOS_SUBDIR / pkg.name
+            github_package = pkg.name.replace("+", "-plus")
             git_url = "https://raxod502:{}@github.com/emacs-straight/{}.git".format(
                 ACCESS_TOKEN, github_package
             )
             repo_obj = org.get_repo(github_package)
             push_git_repo(git_url, repo_dir, repo_obj)
-            log("----> update repo description for package {}".format(package))
-            github_package = package.replace("+", "-plus")
+            log("----> update repo description for package {}".format(pkg.name))
             repo_obj.edit(
                 description="Mirror of the {} package from GNU ELPA, current as of {}".format(
-                    package,
-                    brief_timestamp,
+                    pkg.name,
+                    timestamp.strftime("%Y-%m-%d"),
                 )
             )
-    git_url = "https://raxod502:{}@github.com/emacs-straight/{}.git".format(
-        ACCESS_TOKEN, "gnu-elpa-mirror"
-    )
-    repo_dir = REPOS_SUBDIR / "gnu-elpa-mirror"
-    if "gnu-elpa-mirror" not in existing_repos:
-        log("--> create mirror list repository")
-        org.create_repo(
-            "gnu-elpa-mirror",
-            description="List packages mirrored from GNU ELPA",
-            homepage="https://elpa.gnu.org/packages/",
-            has_issues=False,
-            has_wiki=False,
-            has_projects=False,
-            auto_init=False,
+    if not args.skip_mirror_index:
+        git_url = "https://raxod502:{}@github.com/emacs-straight/{}.git".format(
+            ACCESS_TOKEN, "gnu-elpa-mirror"
         )
-    log("--> clone/update mirror list repository")
-    clone_git_repo(git_url, repo_dir, private_url=True)
-    log("--> update mirror list repository")
-    delete_contents(repo_dir)
-    for package in packages:
-        with open(repo_dir / package, "w"):
-            pass
-    stage_and_commit(repo_dir, make_commit_message("Update mirror list", commit_data))
-    log("--> push changes to mirror list repository")
-    repo = org.get_repo("gnu-elpa-mirror")
-    push_git_repo(git_url, repo_dir, repo_obj=repo)
-    log("--> update repo description for mirror list repository")
-    repo.edit(description="List packages mirrored from GNU ELPA")
+        repo_dir = REPOS_SUBDIR / "gnu-elpa-mirror"
+        if "gnu-elpa-mirror" not in existing_repos:
+            log("--> create mirror list repository")
+            org.create_repo(
+                "gnu-elpa-mirror",
+                description="List packages mirrored from GNU ELPA",
+                homepage="https://elpa.gnu.org/packages/",
+                has_issues=False,
+                has_wiki=False,
+                has_projects=False,
+                auto_init=False,
+            )
+        log("--> clone/update mirror list repository")
+        clone_git_repo(git_url, repo_dir, private_url=True)
+        log("--> update mirror list repository")
+        delete_contents(repo_dir)
+        for pkg in elpa_packages:
+            with open(repo_dir / pkg.name, "w"):
+                pass
+        stage_and_commit(repo_dir, make_commit_message("Update mirror list", timestamp))
+        log("--> push changes to mirror list repository")
+        repo = org.get_repo("gnu-elpa-mirror")
+        push_git_repo(git_url, repo_dir, repo_obj=repo)
+        log("--> update repo description for mirror list repository")
+        repo.edit(description="List packages mirrored from GNU ELPA")
 
 
 def mirror_emacsmirror(_, api, existing_repos):
@@ -551,7 +518,7 @@ def mirror_emacsmirror(_, api, existing_repos):
                 else:
                     assert False, line
     assert num_attic >= 500 and num_mirror >= 1000
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     epkgs_commit = (
         subprocess.run(
             ["git", "rev-parse", "HEAD"],
@@ -576,7 +543,7 @@ def mirror_emacsmirror(_, api, existing_repos):
 
 
 def mirror_orgmode(_, api, existing_repos):
-    brief_timestamp = datetime.datetime.now().strftime("%Y-%m-%d")
+    brief_timestamp = datetime.now().strftime("%Y-%m-%d")
     org = api.get_organization("emacs-straight")
     orgmode_dir = REPOS_SUBDIR / "org-mode"
     orgmode_git_url = "https://git.savannah.gnu.org/git/emacs/org-mode.git"
@@ -618,6 +585,7 @@ def mirror():
     parser = argparse.ArgumentParser()
     parser.add_argument("--skip-gnu-elpa", action="store_true")
     parser.add_argument("--skip-emacsmirror", action="store_true")
+    parser.add_argument("--skip-mirror-index", action="store_true")
     parser.add_argument("--skip-mirror-pulls", action="store_true")
     parser.add_argument("--skip-mirror-pushes", action="store_true")
     parser.add_argument("--skip-orgmode", action="store_true")
